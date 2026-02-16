0d2d42fc137a36c6aac70e09fc28ac6c6b13790b
luiwebdev <148648445+luiwebdev@users.noreply.github.com>
2026-02-15 22:54:32 -0800

    Complete reverseString

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,4 +1,12 @@
-const reverseString = function() {
+const reverseString = function(input) {
+    // hello
+    let string = "";
+
+    for (let i = input.length - 1; i >= 0; i--) {
+        string = string + input[i];
+    }
+
+    return string;
 
 };
 
