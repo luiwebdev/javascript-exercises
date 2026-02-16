a25dbf41338d2dafcdd3687b79f141b2259e84c6
luiwebdev <148648445+luiwebdev@users.noreply.github.com>
2026-02-16 11:46:38 -0800

    Complete sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,4 +1,18 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    let sum = 0;
+
+    if (start > end) {
+        for (let i = start; i >= end; i--) {
+            sum = sum + i;
+        }
+    } else {
+        for (let i = start; i <= end; i++) {
+            sum = sum + i;
+        }
+    }
+
+    return sum;
+
 
 };
 
