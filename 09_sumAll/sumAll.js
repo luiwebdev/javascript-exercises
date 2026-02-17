5b8a35e2852b931b9674c14dff24702760cb1da3
luiwebdev <148648445+luiwebdev@users.noreply.github.com>
2026-02-17 10:39:09 -0800

    Complete test sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,19 +1,24 @@
-const sumAll = function(start, end) {
+const sumAll = function (start, end) {
     let sum = 0;
 
+    if (start < 0 || end < 0) {
+        return "ERROR";
+    }
+    if (!Number.isInteger(start) || !Number.isInteger(end)) {
+        return "ERROR";
+    }
     if (start > end) {
         for (let i = start; i >= end; i--) {
             sum = sum + i;
         }
-    } else {
+    }
+    if (start < end) {
         for (let i = start; i <= end; i++) {
             sum = sum + i;
         }
     }
 
     return sum;
-
-
 };
 
 // Do not edit below this line
