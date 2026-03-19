76a954b63a461c95963becf6c1771f34ba9e9cbe
luiwebdev <148648445+luiwebdev@users.noreply.github.com>
2026-03-18 23:45:57 -0700

    Complete calculator exercise

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((total, item) => {
+    return total + item;
+  }, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((total, item) => {
+    return total * item;
+  }, 1)
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return base ** exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let facto = 1;
+	for (let i = 2; i <= a; i++) {
+    facto = facto * i;
+  }
+
+  return facto;
 };
 
 // Do not edit below this line
