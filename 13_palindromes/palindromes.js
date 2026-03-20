4a75807845d8f36a2d4cb778df13550faa373fbd
luiwebdev <148648445+luiwebdev@users.noreply.github.com>
2026-03-19 23:29:30 -0700

    Complete palindromes exercise

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,4 +1,31 @@
-const palindromes = function () {
+const palindromes = function (str) {
+
+
+    const strLower = str.toLowerCase();
+    const strArray = strLower.split("");
+    const remove = [".", " ", ",", "!"];
+
+    const strNew = strArray.filter((item) => {
+        return !remove.includes(item);
+    })
+
+    const strCopy = strArray.filter((item) => {
+        return !remove.includes(item);
+    })
+
+    const strReversed = strCopy.reverse();
+
+
+    for (let i = 0; i < strNew.length; i++) {
+        if (strNew[i] !== strReversed[i]) {
+            return false
+        }
+    }
+
+    return true;
+
+
+
 
 };
 
