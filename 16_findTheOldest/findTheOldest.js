3ecb129ab4d9a56732f021f26e9eb35ee08e6d62
luiwebdev <148648445+luiwebdev@users.noreply.github.com>
2026-03-27 13:32:36 -0700

    Complete find the oldest person exercise

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,4 +1,29 @@
-const findTheOldest = function() {
+const findTheOldest = function (arr) {
+
+
+    const age = arr.map((item) => {
+        if (!item.hasOwnProperty("yearOfDeath")) {
+            item.yearOfDeath = 2026;
+        }
+        return item.yearOfDeath - item.yearOfBirth;
+    })
+
+    const oldestAge = age.reduce((acc, item) => {
+        if (item > acc) {
+            return item;
+        } else {
+            return acc
+        }
+    }, age[0]);
+
+    const oldestPerson = arr.find((item) => {
+        if (item.yearOfDeath - item.yearOfBirth == oldestAge) {
+            return item;
+        }
+    });
+
+
+    return oldestPerson;
 
 };
 
